ff8fbe4dd8a89db12dfb90c2c800c4ba9179bbaa
plainfingers <plainfingers@me.com>
2012-03-20 20:31:18 -0500

    Initial Commit

---
+++ ShortCo/ShortCoAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  ShortCoAppDelegate.h
+//  ShortCo
+//
+//  Created by Chad Gulley on 3/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ShortCoAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ ShortCoTests/ShortCoTests.h
@@ -0,0 +1,13 @@
+//
+//  ShortCoTests.h
+//  ShortCoTests
+//
+//  Created by Chad Gulley on 3/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ShortCoTests : SenTestCase
+
+@end
