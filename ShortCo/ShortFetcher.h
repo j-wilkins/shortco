441aefb9cf73c32a61f67f05156447667952865b
jake <jake.wilkins@adfitech.com>
2012-03-22 00:56:48 -0500

    refactor some stuff.

--- ShortCo/ShortFetcher.h
+++ ShortCo/ShortFetcher.h
@@ -13,8 +13,31 @@
 
 @interface ShortFetcher : NSObject <NSXMLParserDelegate> {
     NSString *shortenedUrl;
+    NSString *shortenerUrl;
+    NSMutableDictionary *configuration;
     SBJsonParser *_parser;
     SBJsonWriter *_writer;
 }
+
 - (NSString *)fetchShortUrl:(NSString *)url withError:(NSError **)outError;
+
+
+- (NSData *)generatePostParams:(NSString *)url withOptions:(NSMutableDictionary *)options;
+
+- (NSData *)generatePostParamsForUrl:(NSString *)url;
+
+
+- (NSData *)makeUrlRequest:(NSString *)ofType toUrl:(NSString *)url withParams:
+    (NSData *)params withError:(NSError **)outError;
+
+
+
+- (NSString *)generateURLStringForAction:(NSString *)action;
+
+- (NSString *)generateResultURLString:(NSString *)shortened;
+
+
+
+- (void)defineConfigurations;
+
 @end
--- ShortCo/ShortFetcher.m
+++ ShortCo/ShortFetcher.m
@@ -13,37 +13,115 @@
 
 - (NSString *)fetchShortUrl:(NSString *)urlString withError:(NSError **)outError
 {
-    _parser = [[SBJsonParser alloc] init];
-    _writer = [[SBJsonWriter alloc] init];
-    
-    // yikes. all this seems to be the equivalent of:
-    // parmsDict = {"shortener" => {"url" => urlString}} in ruby...
-    NSMutableDictionary *urlDict = [[NSMutableDictionary alloc] init];
-    [urlDict setObject:urlString forKey:@"url"];
 
-    NSString *post = [NSString stringWithFormat:@"shortener=%@", [_writer stringWithObject:urlDict]];
-    NSData *parmsData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+    [self defineConfigurations];
+    
+    NSData *parmsData = [self generatePostParamsForUrl:urlString];
     
     
     NSLog(@"fetching %@", urlString);
-//    NSURL *url = [NSURL URLWithString:@"http://localhost:9292/add.json"];
-    NSURL *url = [NSURL URLWithString:@"http://shortener1.heroku.com/add.json"];
-    NSMutableURLRequest *req = [NSMutableURLRequest requestWithURL:url];
-    [req setHTTPMethod:@"POST"];
-    [req setHTTPBody:parmsData];
     
-    NSHTTPURLResponse *resp = nil;
-    NSData *data = [NSURLConnection sendSynchronousRequest:req 
-                                         returningResponse:&resp
-                                                     error:outError];
+    NSString *urlActionString = [self generateURLStringForAction:@"ADD"];
+    
+    NSData *data = [self makeUrlRequest:@"POST" toUrl:urlActionString
+                             withParams:parmsData withError:outError];
+
     NSDictionary *parsedDict = [_parser objectWithData:data];
+
     NSLog(@"data [%@]", [parsedDict valueForKey:@"shortened"]);
-    NSLog(@"response code %ld", [resp statusCode ]);
+
     if (parsedDict != nil) {
-        return [NSString stringWithFormat:@"http://shortener1.heroku.com/%@",
-                [parsedDict valueForKey:@"shortened"]];
+        return [self generateResultURLString:[parsedDict valueForKey:@"shortened"]];
     }
     return nil;
+}
+
+- (NSData *)generatePostParams:(NSString *)url 
+                   withOptions:(NSMutableDictionary *)options
+{
+    NSLog(@"received url string %@", url);
+    
+    [options setObject:url forKey:@"url"];
+    
+    NSLog(@"set options key url to %@", [options objectForKey:@"url"]);
+
+    NSString *jsonString = [_writer stringWithObject:options];
+    
+    NSLog(@"generated jsonString: %@", jsonString);
+
+    NSString *postParamsString;    
+    postParamsString = [NSString stringWithFormat:@"shortener=%@", jsonString];
+    
+    NSLog(@"generated postParamsString: %@", postParamsString);
+    
+    NSData *returnData = [postParamsString dataUsingEncoding:NSASCIIStringEncoding 
+                                        allowLossyConversion:YES];
+    return returnData;
+}
+
+- (NSData *)generatePostParamsForUrl:(NSString *)url
+{
+    NSMutableDictionary *emptyDict = [[NSMutableDictionary alloc] init];
+    NSData *returnValue = [self generatePostParams:url withOptions:emptyDict];
+    return returnValue;
+}
+
+- (NSData *)makeUrlRequest:(NSString *)ofType 
+                     toUrl:(NSString *)urlString 
+                withParams:(NSData *)params 
+                 withError:(NSError *__autoreleasing *)outError
+{
+    
+    NSData *returnData;
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSMutableURLRequest *req = [NSMutableURLRequest requestWithURL:url];
+    
+    if (ofType == @"POST")
+        [req setHTTPMethod:@"POST"];
+    
+    [req setHTTPBody:params];
+    
+    NSHTTPURLResponse *resp = nil;
+    returnData = [NSURLConnection sendSynchronousRequest:req 
+                                       returningResponse:&resp
+                                                   error:outError];
+    return returnData;
+}
+
+- (NSString *)generateURLStringForAction:(NSString *)action
+{
+    NSString *actionUrl = [NSString stringWithFormat:@"%@/%@", 
+                           [configuration objectForKey:@"SHORTENER_URL"],
+        [[configuration objectForKey:@"URL_MAPPINGS"] objectForKey:action]];
+    
+    NSLog(@"returning actionUrl %@", actionUrl);
+    
+    return actionUrl;
+}
+
+- (NSString *)generateResultURLString:(NSString *)shortened
+{
+    NSString *resultString = [NSString stringWithFormat:@"%@/%@",
+                              [configuration objectForKey:@"SHORTENER_URL"], shortened];
+    return resultString;
+}
+
+- (void)defineConfigurations
+{
+    NSMutableDictionary *urlMappings = [[NSMutableDictionary alloc] init];
+    [urlMappings setObject:@"add.json" forKey:@"ADD"];
+    [urlMappings setObject:@"delete.json" forKey:@"DELETE"];
+    
+    _parser = [[SBJsonParser alloc] init];
+    _writer = [[SBJsonWriter alloc] init];
+    
+    configuration = [[NSMutableDictionary alloc] init];
+    [configuration setObject:urlMappings forKey:@"URL_MAPPINGS"];
+    [configuration setObject:@"http://j-b.us" forKey:@"SHORTENER_URL"];
+    
+    NSLog(@"CONFIGURING:: set URL_MAPPINGS[ADD]: %@", [[configuration objectForKey:@"URL_MAPPINGS"] objectForKey:@"ADD"]);
+    NSLog(@"CONFIGURING:: set SHORTENER_URL: %@", [configuration objectForKey:@"SHORTENER_URL"]);
+    
 }
 
 @end
